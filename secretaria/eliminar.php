fd319727c5fe60df518370a0b9ec9c640d518fc2
Edgar Manriquez <edgar_manriquez@ucol.mx>
2015-01-14 10:28:13 -0600

    Update eliminar.php

--- secretaria/eliminar.php
+++ secretaria/eliminar.php
@@ -2,6 +2,6 @@
 $no_trabajador=$_GET['no_trabajador'];
 include('Conexionbd/conexion.php');
 $link=conection();
-$sqlelim=mysql_query("delete from usuarios where no_trabajador='".$no_trabajador."';",$link);
+$sqlelim=mysql_query ("delete from usuarios where no_trabajador='".$no_trabajador."';",$link);
 header('location: form_lista_usuarios.php');
 ?>
